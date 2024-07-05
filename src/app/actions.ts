8715eeaad17689cb42b2f5433e0ed69e01fe7b7f
Ryo Igarashi <n33t5hin@gmail.com>
2024-07-05 19:38:56 +0900

    Use native fetch API

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,27 +1,28 @@
 "use server";
 
-import { HfInferenceEndpoint } from "@huggingface/inference";
+export type Result =
+  | {
+      type: "error";
+      message: string;
+    }
+  | {
+      type: "ok";
+      text: string;
+    };
 
-export type Result = {
-  type: 'error',
-  message: string,
-} | {
-  type: 'ok',
-  text: string,
-}
-
-export async function translate(_prevData: unknown, formData: FormData): Promise<Result> {
-  const inference = new HfInferenceEndpoint(process.env.HF_ENDPOINT!, process.env.HF_TOKEN)
-
+export async function translate(
+  _prevData: unknown,
+  formData: FormData
+): Promise<Result> {
   const text = formData.get("text");
   const direction = formData.get("direction");
   const pronoun = formData.get("pronoun") ?? "first";
-  const dialect = formData.get("dialect") ?? "沙流"
+  const dialect = formData.get("dialect") ?? "沙流";
 
   if (typeof text !== "string" || text.length === 0) {
     return {
       type: "error",
-      message: "テキストが入力されていません。"
+      message: "テキストが入力されていません。",
     };
   }
 
@@ -29,51 +30,57 @@ export async function translate(_prevData: unknown, formData: FormData): Promise
   if (text.length > MAX_LENGTH) {
     return {
       type: "error",
-      message: `テキストの長さが制限を超えています。${MAX_LENGTH}文字以内にしてください。`
-    }
+      message: `テキストの長さが制限を超えています。${MAX_LENGTH}文字以内にしてください。`,
+    };
   }
 
   if (typeof direction !== "string") {
     return {
       type: "error",
-      message: "翻訳方向が不正です。"
-    }
+      message: "翻訳方向が不正です。",
+    };
   }
 
-  let prompt: string = ""
+  let prompt: string = "";
   if (direction === "ja2ain") {
-    prompt = `translate: Japanese to Ainu (${dialect}, ${pronoun}): ${text}`
+    prompt = `translate: Japanese to Ainu (${dialect}, ${pronoun}): ${text}`;
   } else {
-    prompt = `translate: Ainu (${dialect}, ${pronoun}) to Japanese: ${text}`
+    prompt = `translate: Ainu (${dialect}, ${pronoun}) to Japanese: ${text}`;
   }
 
   try {
-    const result = await inference.request({
-      model: "aynumosir/mt5-small-ainu",
-      inputs: prompt,
-      parameters: {
-        max_length: 128,
-        early_stopping: true,
+    const response = await fetch(process.env.HF_ENDPOINT!, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.HF_TOKEN}`,
       },
+      body: JSON.stringify({
+        model: "aynumosir/mt5-small-ainu",
+        inputs: [prompt],
+        parameters: {
+          max_length: 128,
+          early_stopping: true,
+        },
+      }),
     });
 
-
-    if (!Array.isArray(result)) {
-      return {
-        type: "error",
-        message: "翻訳に失敗しました。"
-      }
+    if (!response.ok) {
+      throw new Error("Failed to fetch", { cause: response });
     }
-  
+
+    const data = await response.json();
+
     return {
       type: "ok",
-      text: result[0].translation_text
-    }
+      text: data?.[0]?.["translation_text"],
+    };
   } catch (error) {
-    console.error(error)
+    console.error(error);
     return {
       type: "error",
-      message: "翻訳に失敗しました。サーバーが再起動中の場合がありますので、２〜３分ほど待ってから再度お試しください。"
-    }
+      message:
+        "翻訳に失敗しました。サーバーが再起動中の場合がありますので、２〜３分ほど待ってから再度お試しください。",
+    };
   }
 }
