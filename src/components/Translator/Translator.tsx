31150facbe5cd45a0a3a96c8c8e23a360e615c66
Ryo Igarashi <n33t5hin@gmail.com>
2024-07-05 16:47:36 +0900

    Update the list of dialects

--- src/components/Translator/Translator.tsx
+++ src/components/Translator/Translator.tsx
@@ -96,17 +96,17 @@ export const Translator: FC<TranslatorProps> = (props) => {
               <option value="沙流">沙流</option>
               <option value="千歳">千歳</option>
               <option value="鵡川">鵡川</option>
+              <option value="幌別">幌別</option>
             </optgroup>
 
             <optgroup label="北海道・北東">
               <option value="静内">静内</option>
+              <option value="十勝">十勝</option>
+              <option value="釧路">釧路</option>
               <option value="様似">様似</option>
               <option value="美幌">美幌</option>
-              <option value="旭川">旭川</option>
-            </optgroup>
-
-            <optgroup label="樺太">
-              <option value="樺太">樺太</option>
+              <option value="石狩">石狩</option>
+              <option value="阿寒">阿寒</option>
             </optgroup>
           </Select>
 
