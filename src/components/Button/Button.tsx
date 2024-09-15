808dbd364f08b20bdb4d4a602c4d4645674fefa7
Ryo Igarashi <n33t5hin@gmail.com>
2024-09-15 13:03:41 +0900

    Whiten primary button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,13 +7,19 @@ export const Button: FC<ComponentProps<"button">> = (props) => {
   return (
     <button
       className={clsx(
-        "text-white px-3 py-1.5 rounded-lg",
+        "px-3 py-1.5 rounded-lg",
         "outline-blue-400 outline-2 focus:outline outline-offset-4",
-        "disabled:opacity-70 disabled:hover:bg-blue-500",
-        "bg-blue-600 hover:bg-blue-700",
-        "contrast-more:bg-blue-700 contrast-more:hover:bg-blue-800",
-        "forced-colors:border forced-colors:border-[ButtonBorder]",
         "transition",
+        // light
+        "bg-zinc-950 text-white",
+        "hover:bg-zinc-700",
+        "disabled:bg-zinc-600 disabled:hover:bg-zinc-500",
+        // dark
+        "dark:bg-zinc-50 dark:text-black",
+        "dark:hover:bg-zinc-200",
+        "dark:disabled:bg-zinc-300 dark:disabled:hover:bg-zinc-400",
+        // contrast
+        "forced-colors:border forced-colors:border-[ButtonBorder]",
         className,
       )}
       {...rest}
