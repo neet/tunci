9544b71025288d93666f5ca4136ccab25e03596c
Ryo Igarashi <n33t5hin@gmail.com>
2024-09-27 23:52:59 +0900

    Update banner design

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 import { Metadata } from "next";
-import { getTranslations } from "next-intl/server";
+import { getTranslations, unstable_setRequestLocale } from "next-intl/server";
 
 export type AboutPageProps = {
   params: { locale: string };
@@ -20,19 +20,28 @@ export async function generateMetadata(
 }
 
 export default async function AboutPage({ params }: AboutPageProps) {
+  unstable_setRequestLocale(params.locale);
+
+  const t = await getTranslations("app.AboutPage");
   const Content = (await import(`./${params.locale}.mdx`)).default;
 
   return (
-    <article
-      className={clsx(
-        "bg-white dark:bg-black",
-        "p-4 rounded mx-auto",
-        "border border-zinc-300 dark:border-zinc-700 rounded-lg",
-        "prose-headings:font-serif",
-        "prose prose-zinc prose-a:text-blue-600 prose-a:font-normal dark:prose-invert dark:prose-a:text-blue-400",
-      )}
-    >
-      <Content />
-    </article>
+    <main>
+      <header
+        className={clsx(
+          "max-w-screen-lg mx-auto my-12",
+          "flex flex-col items-center gap-3",
+          "px-4 md:px-0",
+        )}
+      >
+        <h2 className="block text-2xl font-bold">{t("title")}</h2>
+      </header>
+
+      <article className="bg-white dark:bg-black border-y border-zinc-300 dark:border-zinc-700">
+        <div className="max-w-screen-sm mx-auto p-4 prose prose-zinc dark:prose-invert prose-a:text-blue-600 dark:prose-a:text-blue-400 prose-a:font-normal">
+          <Content />
+        </div>
+      </article>
+    </main>
   );
 }
