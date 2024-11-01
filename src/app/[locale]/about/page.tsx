e1e92666d3bde1d9f537594973bd4bb311b7c243
Ryo Igarashi <n33t5hin@gmail.com>
2024-11-02 04:43:43 +0900

    chore: Refactor /about

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -26,21 +26,13 @@ export default async function AboutPage({ params }: AboutPageProps) {
   const Content = (await import(`./${params.locale}.mdx`)).default;
 
   return (
-    <main className="w-full">
-      <header
-        className={clsx(
-          "max-w-screen-lg mx-auto my-12",
-          "flex flex-col items-center gap-3",
-          "px-4 md:px-0",
-        )}
-      >
-        <h2 className="block text-2xl font-bold">{t("title")}</h2>
+    <main className="w-full max-w-screen-sm mx-auto">
+      <header className={clsx("my-12", "px-4 md:px-0")}>
+        <h2 className="block text-2xl font-bold text-center">{t("title")}</h2>
       </header>
 
-      <article className="bg-white dark:bg-black">
-        <div className="max-w-screen-sm mx-auto p-4 prose prose-gray dark:prose-invert prose-a:text-indigo-600 dark:prose-a:text-indigo-400 prose-a:font-normal overflow-clip">
-          <Content />
-        </div>
+      <article className="bg-white dark:bg-black mx-auto p-4 prose prose-gray dark:prose-invert prose-a:text-indigo-600 dark:prose-a:text-indigo-400 prose-a:font-normal overflow-clip">
+        <Content />
       </article>
     </main>
   );
