c86f34b7908ec33ebc8f4c260d75a17b3472e1a8
Ryo Igarashi <n33t5hin@gmail.com>
2025-05-20 14:21:09 +0900

    fix: Add tooltip to IconButtons

--- src/components/Composer/ComposerCollapsible.tsx
+++ src/components/Composer/ComposerCollapsible.tsx
@@ -1,5 +1,14 @@
 import * as Collapsible from "@radix-ui/react-collapsible";
-import { Card, ChevronDownIcon, Flex, Heading, Reset } from "@radix-ui/themes";
+import {
+  Card,
+  ChevronDownIcon,
+  Flex,
+  Heading,
+  IconButton,
+  Tooltip,
+  VisuallyHidden,
+} from "@radix-ui/themes";
+import { useTranslations } from "next-intl";
 import { FC, ReactNode, useId, useState } from "react";
 
 export type ComposerCollapsibleProps = {
@@ -12,28 +21,32 @@ export const ComposerCollapsible: FC<ComposerCollapsibleProps> = (props) => {
 
   const [open, setOpen] = useState(true);
   const id = useId();
+  const t = useTranslations("components.Composer.ComposerCollapsible");
 
   return (
     <Card role="region" aria-labelledby={id} size="2">
       <Collapsible.Root open={open} onOpenChange={setOpen}>
         <Flex direction="column" gap="3">
-          <Collapsible.Trigger asChild>
-            <Flex justify="between" align="center" width="100%" asChild>
-              <Reset>
-                <button style={{ width: "100%" }}>
-                  <Heading as="h3" size="3" id={id}>
-                    {title}
-                  </Heading>
+          <Flex justify="between" align="center" width="100%">
+            <Heading as="h3" size="3" id={id}>
+              {title}
+            </Heading>
 
+            <Tooltip content={open ? t("collapse") : t("expand")}>
+              <Collapsible.Trigger asChild>
+                <IconButton variant="ghost" color="gray">
                   <ChevronDownIcon
                     style={{
                       transform: open ? "rotate(180deg)" : "rotate(0deg)",
                     }}
                   />
-                </button>
-              </Reset>
-            </Flex>
-          </Collapsible.Trigger>
+                  <VisuallyHidden>
+                    {open ? t("collapse") : t("expand")}
+                  </VisuallyHidden>
+                </IconButton>
+              </Collapsible.Trigger>
+            </Tooltip>
+          </Flex>
 
           <Collapsible.Content>{children}</Collapsible.Content>
         </Flex>
