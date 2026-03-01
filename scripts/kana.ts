7d79ecd691ef887c7d2250e67b552c4cb95f305f
Ryo Igarashi <n33t5hin@gmail.com>
2026-03-01 12:24:20 +0900

    feat: Automatically create PR upading ain-Kana.json

--- scripts/kana.ts
+++ scripts/kana.ts
@@ -1,14 +1,48 @@
+/* eslint-disable no-control-regex */
+import { default as ainu } from "ainu-utils";
 import fs from "fs/promises";
 import path from "path";
 
-import { convert } from "./kana_converter";
+const MUSTACHE = /\{(.+?)\}/g;
+const HTML_TAG = /<[^>]+>/g;
+const NON_ASCII = /[^\x00-\x7F]+/g;
+const SYMBOL = /\x00([0-9]+)\x00/g;
 
-type File = {
-  source: string;
-  target: string;
-};
+function safeConvertToKana(text: string) {
+  let counter = 0;
+  const registry = new Map<string, string>();
 
-const files: File[] = [
+  const escape = (match: string) => {
+    const key = `\x00${counter++}\x00`;
+    registry.set(key, match);
+    return key;
+  };
+
+  let safeText = text.replace(MUSTACHE, escape);
+  safeText = safeText.replace(HTML_TAG, escape);
+  safeText = safeText.replace(NON_ASCII, escape);
+  safeText = safeText.replace(/'/g, "'");
+  safeText = safeText.replace(/’/g, "'");
+
+  const kana = ainu.to_kana(safeText);
+
+  return kana.replace(SYMBOL, (match, p1) => {
+    return registry.get(`\x00${p1}\x00`) || match;
+  });
+}
+
+function convert(record: Record<string, unknown>) {
+  return Object.fromEntries(
+    Object.entries(record).map(([key, value]): [string, unknown] => {
+      if (typeof value === "string") {
+        return [key, safeConvertToKana(value)];
+      }
+      return [key, convert(value as Record<string, unknown>)];
+    }),
+  );
+}
+
+const files = [
   {
     source: path.join(__dirname, "../messages/ain-Latn.json"),
     target: path.join(__dirname, "../messages/ain-Kana.json"),
@@ -16,25 +50,14 @@ const files: File[] = [
 ];
 
 async function main() {
-  const checkMode = !!process.env.CHECK;
-
   for (const file of files) {
     const source = await fs.readFile(file.source, "utf-8");
-    const target = await fs.readFile(file.target, "utf-8");
 
     if (path.extname(file.source) === ".json") {
       const sourceJson = JSON.parse(source);
       const content = convert(sourceJson);
       const contentJSON = JSON.stringify(content, null, 2) + "\n";
-
-      if (checkMode) {
-        if (contentJSON !== target) {
-          console.error(`[ERROR] ${file.source}`);
-          process.exit(1);
-        }
-      } else {
-        await fs.writeFile(file.target, contentJSON);
-      }
+      await fs.writeFile(file.target, contentJSON);
     }
   }
 }
