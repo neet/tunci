54188443e8eed27822b54664c920bb193f3c1382
Ryo Igarashi <n33t5hin@gmail.com>
2026-03-04 23:05:18 +0900

    fix: Set longer lifetime to NEXT_LOCALE

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -4,6 +4,11 @@ import { defineRouting } from "next-intl/routing";
 export const routing = defineRouting({
   locales: ["ain-Latn", "ain-Kana", "ja"] as string[],
   defaultLocale: "ain-Latn",
+
+  // https://next-intl.dev/docs/routing/configuration#locale-cookie
+  localeCookie: {
+    maxAge: 60 * 60 * 24 * 365,
+  },
 });
 
 export const { Link, redirect, usePathname, useRouter } =
